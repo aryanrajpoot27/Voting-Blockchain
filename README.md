80c7bbebb0ad2b7c3ab3d0c7267c9146559ae6b6
Aryan Rajpoot <araryan1212@gmail.com>
2024-04-21 01:26:58 +0530

    voting

---
+++ README.md
@@ -0,0 +1,12 @@
+```shell
+npm install
+```
+
+```shell
+npx hardhat compile
+npx hardhat run --network sepolia scripts/deploy.js
+```
+
+```shell
+npm start
+```
---
+++ src/Components/Finished.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from './Login';
+const Finished = (props) => {
+    return (
+        <div className="login-container">
+            <h1 className="welcome-message">Voting is Finished</h1>
+        </div>
+    
+    )
+}
+
+export default Finished;
